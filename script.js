adebc830398fa6ac1f32bcedc9dd00d5318d4729
Lyxnx <a.julnes@gmail.com>
2020-01-29 21:10:34 +0000

    remove old files

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleId(id) {
-	var e = document.getElementById(id);
-	var disp = getComputedStyle(e, null).display;
-	if (disp !== 'none')
-		e.style.display = 'none';
-	else
-		e.style.display = 'block';
-}
